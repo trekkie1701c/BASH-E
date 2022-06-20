9abe23728d1c80692c587924c13fe41528322c01
trekkie1701c <trekkie1701c@gmail.com>
2022-06-20 16:05:10 -0700

    Fix Size

--- backend/ascii.py
+++ backend/ascii.py
@@ -4,5 +4,5 @@ from PIL import Image
 from picharsso import new_drawer
 
 def convert(image):
-	drawer = new_drawer("braille", height=256, colorize=True)
+	drawer = new_drawer("braille", height=64, colorize=True)
 	print(drawer(image))
